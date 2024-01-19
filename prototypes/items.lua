1a55ef76b49202da2a0b39656f8ba6340e8b0613
BuildTools <unconfigured@null.spigotmc.org>
2024-01-20 00:54:43 +0100

    Removed shit files

---
+++ prototypes/items.lua
@@ -0,0 +1,126 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "surface-driller",
+    icon = "__base__/graphics/icons/assembling-machine-1.png",
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "production-machine",
+    order = "a[assembling-machine-1]",
+    place_result = "surface-driller",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "item-elevator-input",
+    icon = "__Subsurface__/graphics/icons/items-elevator-icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "inter-surface-transport",
+    order = "c[transport-belt]-c[items-elevator-mk1]",
+    place_result = "item-elevator-input",
+    stack_size = 10
+  },
+  {
+    type = "item",
+    name = "item-elevator-output",
+    icon = "__Subsurface__/graphics/icons/items-elevator-icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "inter-surface-transport",
+    order = "c[transport-belt]-c[items-elevator-mk1]",
+    place_result = "item-elevator-output",
+    stack_size = 10
+  },
+  {
+    type = "item",
+    name = "fluid-elevator-input",
+    icon = "__Subsurface__/graphics/icons/fluid_elevator_mk1_icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "inter-surface-transport",
+    order = "d[fluid]-a[fluid-elevator]",
+    place_result = "fluid-elevator-input",
+    stack_size = 10
+  },
+  {
+    type = "item",
+    name = "fluid-elevator-output",
+    icon = "__Subsurface__/graphics/icons/fluid_elevator_mk1_icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "inter-surface-transport",
+    order = "d[fluid]-a[fluid-elevator]",
+    place_result = "fluid-elevator-output",
+    stack_size = 10
+  },
+  {
+    type = "item",
+    name = "air-vent",
+    icon = "__Subsurface__/graphics/icons/air_vent_11_icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "inter-surface-transport",
+    order = "a[air-vent-passive]",
+    place_result = "air-vent",
+    stack_size = 50
+  }, 
+  {
+    type = "item",
+    name = "active-air-vent",
+    icon = "__Subsurface__/graphics/icons/air_vent_22_icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "inter-surface-transport",
+    order = "b[air-vent-active]",
+    place_result = "active-air-vent",
+    stack_size = 50
+  },
+  --[[{
+    type = "item",
+    name = "mobile-borer",
+    icon = "__base__/graphics/icons/car.png",
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "transport",
+    order = "b[personal-transport]-a[car]",
+    place_result = "mobile-borer",
+    stack_size = 1
+  },
+
+  {
+    type = "item",
+    name = "digging-planner",
+    icon = "__Subsurface__/graphics/icons/digging-planner-icon.png",
+    icon_size = 32, icon_mipmaps = 1,
+    subgroup = "tool",
+    order = "c[automated-construction]-c[digging-planner]",
+    place_result = "selection-marker",
+    stack_size = 1
+  },
+  {
+    type = "item",
+    name = "digging-robots-deployment-center",
+    icon = "__base__/graphics/icons/assembling-machine-1.png",
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "production-machine",
+    order = "a[assembling-machine-1]",
+    place_result = "digging-robots-deployment-center",
+    stack_size = 50
+  },
+
+  {
+    type = "item",
+    name = "assembled-digging-robots",
+    icon = "__base__/graphics/icons/assembling-machine-1.png",
+    icon_size = 64, icon_mipmaps = 4,
+    flags = {},
+    subgroup = "production-machine",
+    order = "a[assembling-machine-1]",
+    stack_size = 50
+  },
+
+  {
+    type = "item",
+    name = "prepared-digging-robots",
+    icon = "__base__/graphics/icons/assembling-machine-1.png",
+    icon_size = 64, icon_mipmaps = 4,
+    flags = {"hidden"},
+    subgroup = "production-machine",
+    order = "a[assembling-machine-1]",
+    stack_size = 1
+  },]]
+})
---
+++ prototypes/tiles.lua
@@ -0,0 +1,119 @@
+data:extend(
+{
+  {
+    type = "tile",
+    name = "caveground",
+    needs_correction = false,
+    mined_sound = { filename = "__base__/sound/deconstruct-bricks.ogg" },
+    collision_mask = {"ground-tile"},
+    walking_speed_modifier = 1.4,
+    layer = 61,
+	pollution_absorption_per_second = 0,
+    variants =
+    {
+      main =
+      {
+        {
+          picture = "__Subsurface__/graphics/terrain/underground-dirt.png",
+          count = 4,
+          size = 1
+        }
+      },
+      inner_corner =
+      {
+        picture = "__Subsurface__/graphics/terrain/underground-dirt-inner-corner.png",
+        count = 1
+      },
+      outer_corner =
+      {
+        picture = "__Subsurface__/graphics/terrain/underground-dirt-outer-corner.png",
+        count = 1
+      },
+      side =
+      {
+        picture = "__Subsurface__/graphics/terrain/underground-dirt-side.png",
+        count = 4
+      },
+      u_transition =
+      {
+        picture = "__Subsurface__/graphics/terrain/underground-dirt-u.png",
+        count = 1
+      },
+      o_transition =
+      {
+        picture = "__Subsurface__/graphics/terrain/underground-dirt-o.png",
+        count = 1
+      }
+    },
+    walking_sound =
+    {
+      {
+        filename = "__base__/sound/walking/dirt-02.ogg",
+        volume = 0.8
+      },
+      {
+        filename = "__base__/sound/walking/dirt-03.ogg",
+        volume = 0.8
+      },
+      {
+        filename = "__base__/sound/walking/dirt-04.ogg",
+        volume = 0.8
+      }
+    },
+    map_color={r=0.5, g=0.332, b=0.144}
+  },
+  {
+    type = "tile",
+    name = "cave-walls",
+    collision_mask =
+    {
+      "ground-tile",
+      --"water-tile",
+      "resource-layer",
+      --"floor-layer",
+      --"item-layer",
+      --"object-layer",
+      --"player-layer",
+      "doodad-layer"
+    },
+    layer = 60,
+	pollution_absorption_per_second = 0,
+    variants =
+    {
+      main =
+      {
+        {
+          picture = "__Subsurface__/graphics/terrain/cave-walls.png",
+          count = 4,
+          size = 1
+        }
+      },
+      inner_corner =
+      {
+        picture = "__Subsurface__/graphics/terrain/cave-walls-inner-corner.png",
+        count = 1
+      },
+      outer_corner =
+      {
+        picture = "__Subsurface__/graphics/terrain/cave-walls-outer-corner.png",
+        count = 1
+      },
+      side =
+      {
+        picture = "__Subsurface__/graphics/terrain/cave-walls-side.png",
+        count = 1
+      },
+      u_transition =
+      {
+        picture = "__Subsurface__/graphics/terrain/cave-walls-u.png",
+        count = 1
+      },
+      o_transition =
+      {
+        picture = "__Subsurface__/graphics/terrain/cave-walls-o.png",
+        count = 1
+      }
+    },
+    map_color={r=0.367, g=0.234, b=0.082}
+  },
+})
