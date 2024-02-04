587bbc390fa57825137838c1b0a962a1a7c9551b
NathaU <72204083+NathaU@users.noreply.github.com>
2024-02-04 14:31:08 +0100

    Support for Rocket Silo Construction Mod

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,8 +6,20 @@ for _,e in pairs(data.raw["locomotive"]) do
 	end
 end
 
+
+-- prevent entities from placing in subsurfaces
+
 for _,e in pairs(data.raw["rocket-silo"]) do
 	local collisions = collision_mask_util.get_mask(e)
 	collision_mask_util.add_layer(collisions, "layer-50")
 	e.collision_mask = collisions
+end
+
+local assemblers = {"rsc-silo-stage1", "rsc-silo-stage1-sesprs", "rsc-silo-stage1-serlp"}
+for _,a in ipairs(assemblers) do
+	if data.raw["assembling-machine"][a] then
+		local collisions = collision_mask_util.get_mask(data.raw["assembling-machine"][a])
+		collision_mask_util.add_layer(collisions, "layer-50")
+		data.raw["assembling-machine"][a].collision_mask = collisions
+	end
 end
