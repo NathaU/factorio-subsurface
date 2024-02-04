8d68656dfac56c953b47f37c04b24ab36781d768
BuildTools <unconfigured@null.spigotmc.org>
2024-02-04 14:10:55 +0100

    Locomotives now produce pollution

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,11 @@
 local collision_mask_util = require("collision-mask-util")
 
+for _,e in pairs(data.raw["locomotive"]) do
+	if e.name ~= "electric-locomotive" and e.name ~= "deg-electric-locomotive" then
+		e.burner.emissions_per_minute = 10
+	end
+end
+
 for _,e in pairs(data.raw["rocket-silo"]) do
 	local collisions = collision_mask_util.get_mask(e)
 	collision_mask_util.add_layer(collisions, "layer-50")
