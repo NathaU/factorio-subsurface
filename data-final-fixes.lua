f92db556bfbcb00f6a29406cc084158703829ff4
Natha <no-reply@factorio.com>
2024-02-08 16:23:36 +0100

    Fixed issue with non-existent locomotive burner prototypes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,8 +1,8 @@
 local collision_mask_util = require("collision-mask-util")
 
 for _,e in pairs(data.raw["locomotive"]) do
-	if e.name ~= "electric-locomotive" and e.name ~= "deg-electric-locomotive" then
-		e.burner.emissions_per_minute = 10
+	if e.name ~= "electric-locomotive" and e.name ~= "deg-electric-locomotive" and e.burner then
+		e.burner.emissions_per_minute = e.burner.emissions_per_minute or 10
 	end
 end
 
