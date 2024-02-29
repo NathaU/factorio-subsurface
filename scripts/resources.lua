b72349b9c451cae3cdfcf5c31408554c98730dba
Natha <no-reply@factorio.com>
2024-02-29 15:26:54 +0100

    Autoplace settings for SE worlds

--- scripts/resources.lua
+++ scripts/resources.lua
@@ -31,6 +31,7 @@ end
 
 -- this is for subsurfaces, it returns a freshly new autoplace_controls array
 -- ensure that all resources really exist!
+-- depth is always >= 1
 function make_autoplace_controls(topname, depth)
 	if topname == "nauvis" then
 		return {
@@ -40,11 +41,41 @@ function make_autoplace_controls(topname, depth)
 			["crude-oil"] = 	{frequency = 1.5*depth, 	size = 1.5*depth, 	richness = 2*depth},
 			["coal"] = 			{frequency = 0.8^depth, 	size = 0.5^depth, 	richness = 0.8^depth},
 		}
+	elseif remote.interfaces["space-exploration"] then
+		local zone = remote.call("space-exploration", "get_zone_from_name", {zone_name = (topname:gsub("^%l", string.upper))})
+		local res_table = {}
+		for _,control in pairs(game.autoplace_control_prototypes) do
+			if control.category == "resource" and zone.controls[control.name] then
+				local zone_control = zone.controls[control.name]
+				local freq = game.get_surface(topname).map_gen_settings.autoplace_controls[control.name].frequency
+				local size = zone_control.size
+				local rich = zone_control.richness
+				if control.name == "crude-oil" or control.name == "adamo-carbon natural-gas" or control.name == "mineral-water" then
+					rich = rich * 0.01
+				end
+				-- This is for mods added after space exploration.
+				if freq == 0 and size == 0 then
+					freq = 1
+					size = 1
+					rich = 1
+				end
+				-- end of exception
+				local res = {
+					frequency = freq * (1 + depth * 0.2),
+					size = size * (1 + depth * 0.2),
+					richness = rich * (1 + depth * 0.2)
+				}
+				game.print(control.name.." old values: "..string.format("%.2f\t%.2f\t%.2f", freq, size, rich))
+				game.print(control.name.." new values: "..string.format("%.2f\t%.2f\t%.2f", res.frequency, res.size, res.richness))
+				res_table[control.name] = res;
+			end
+		end
+		return res_table
 	else return {}
 	end
 end
 
--- When top surfaces are created (but not for nauvis!)
+-- When top surfaces are created (this is not called for nauvis)
 script.on_event(defines.events.on_surface_created, function(event)
 	if not is_subsurface(event.surface_index) then
 		manipulate_autoplace_controls(game.get_surface(event.surface_index))
