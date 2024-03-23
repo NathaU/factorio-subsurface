cf44c9f377ad7958996c7e35a4cb0efa24723d50
Natha <no-reply@factorio.com>
2024-03-23 10:17:47 +0100

    Tunnel entrance and exit is now just a hole

--- scripts/cutscene.lua
+++ scripts/cutscene.lua
@@ -2,11 +2,6 @@ script.on_event(defines.events.on_player_driving_changed_state, function(event)
 	if event.entity and global.car_links[event.entity.unit_number] then
 		local player = game.get_player(event.player_index)
 		local waypoints = {{position=event.entity.position, transition_time=30, time_to_wait=0}}
-		if event.entity.name == "tunnel-entrance" then
-			waypoints[1].position.y = waypoints[1].position.y + 2
-		elseif event.entity.name == "tunnel-exit" then
-			waypoints[1].position.y = waypoints[1].position.y - 2
-		end
 		player.set_controller{type=defines.controllers.cutscene, waypoints=waypoints}
 	end
 end)
@@ -15,10 +10,10 @@ script.on_event(defines.events.on_cutscene_waypoint_reached, function(event)
 	local player = game.get_player(event.player_index)
 	if event.waypoint_index == 0 and player.cutscene_character.vehicle then -- if this is the first part of the whole cutscene
 		player.exit_cutscene()
-		local name = player.vehicle.name
-		player.teleport(player.position, global.car_links[player.vehicle.unit_number].surface)
+		local endpoint_name = player.vehicle.name
+		player.teleport({player.position.x, player.position.y + 1}, global.car_links[player.vehicle.unit_number].surface)
 		local start = player.position
-		if name == "tunnel-entrance" then
+		if endpoint_name == "tunnel-entrance" then
 			start.y = start.y - 2
 		else
 			start.y = start.y + 2
