81288521648a244f71ebd3ce81458a3618bd3f84
Natha <no-reply@factorio.com>
2024-04-01 13:51:42 +0200

    Item elevator is now a linked belt

--- migrations/1.0.7.lua
+++ migrations/1.0.7.lua
@@ -1,6 +1,8 @@
 -- as fluid elevator type changed, they have new unit numbers which means the whole global.fluid_elevators is useless
 global.fluid_elevators = {}
+global.item_elevators = nil
 local linked_endpoints = {}
+
 for _,s in pairs(game.surfaces) do
 	for _,top in ipairs(s.find_entities_filtered{name="fluid-elevator-input"}) do
 		-- since the outer for loop is sorted by surface index, we automatically have the top endpoint first
@@ -18,6 +20,17 @@ for _,s in pairs(game.surfaces) do
 			local bottom = get_subsurface(s, false).find_entity("fluid-elevator-input", top.position)
 			if bottom and not linked_endpoints[top.unit_number] then
 				table.insert(global.fluid_elevators, {bottom, top})
+				linked_endpoints[top.unit_number] = true
+			end
+		end
+	end
+	for _,top in ipairs(s.find_entities_filtered{name="item-elevator"}) do
+		-- since the outer for loop is sorted by surface index, we automatically have the top endpoint first
+		if get_subsurface(s, false) then
+			local bottom = get_subsurface(s, false).find_entity("item-elevator", top.position)
+			if bottom and not linked_endpoints[top.unit_number] then
+				bottom.linked_belt_type = "output"
+				top.connect_linked_belts(bottom)
 				linked_endpoints[top.unit_number] = true
 			end
 		end
--- scripts/elevators.lua
+++ scripts/elevators.lua
@@ -1,15 +1,15 @@
 function switch_elevator(entity) -- switch between input and output
-	local is_fluid = false
-	
-	local new_name = "item-elevator-output"
-	if entity.name == "item-elevator-output" then new_name = "item-elevator-input"
-	elseif entity.name == "fluid-elevator-input" then
-		new_name = "fluid-elevator-output"
-		is_fluid = true
-	elseif entity.name == "fluid-elevator-output" then
-		new_name = "fluid-elevator-input"
-		is_fluid = true
+	if entity.name == "item-elevator" or (entity.name == "entity-ghost" and entity.ghost_name == "item-elevator") then
+		if entity.linked_belt_type == "input" then
+			entity.linked_belt_type = "output"
+		else
+			entity.linked_belt_type = "input"
+		end
+		return
 	end
+	
+	local new_name = "fluid-elevator-input"
+	if entity.name == "fluid-elevator-input" then new_name = "fluid-elevator-output" end
 	
 	-- we need to destroy the old entity first because otherwise the new one won't connect to pipes
 	local surface = entity.surface
@@ -17,33 +17,27 @@ function switch_elevator(entity) -- switch between input and output
 	local direction = entity.direction
 	local force = entity.force
 	local last_user = entity.last_user
-	local fluidbox = false
+	local fluidbox = entity.fluidbox[1]
 	local inv = false
-	if is_fluid then fluidbox = entity.fluidbox[1] end
 	entity.destroy()
 	
 	local new_endpoint = surface.create_entity{name=new_name, position=pos, direction=direction, force=force, player=last_user}
-	if is_fluid then new_endpoint.fluidbox[1] = fluidbox
-	else -- copy items
-	end
+	new_endpoint.fluidbox[1] = fluidbox
 	
 	return new_endpoint
 end
 
-function handle_elevators(tick)
-	
-	-- item elevators
-	for i,elevators in ipairs(global.item_elevators) do  -- move items from input to output
-		if not(elevators[1].valid and elevators[2].valid) then -- remove link and change output to input
-			if elevators[2].valid then switch_elevator(elevators[2]) end
-			table.remove(global.item_elevators, i)
-		else
-			if elevators[1].get_item_count() > 0 and elevators[2].can_insert(elevators[1].get_inventory(defines.inventory.chest)[1]) then
-				elevators[2].insert(elevators[1].get_inventory(defines.inventory.chest)[1])
-				elevators[1].remove_item(elevators[1].get_inventory(defines.inventory.chest)[1])
-			end
+function is_linked(entity)
+	if elevator_name == "item-elevator" then
+		return entity.linked_belt_neighbour ~= nil
+	else
+		for i,v in ipairs(global.fluid_elevators) do
+			if v[1] == entity or v[2] == entity then return true end
 		end
 	end
+end
+
+function handle_elevators(tick)
 	
 	-- fluid elevators
 	for i,elevators in ipairs(global.fluid_elevators) do  -- average fluid between input and output
@@ -65,25 +59,17 @@ function handle_elevators(tick)
 	end
 end
 
-function show_placement_indicators(player, elevator_name, link_key)
+function show_placement_indicators(player, elevator_name)
 	if get_oversurface(player.surface) then
-		for _,e in ipairs(get_oversurface(player.surface).find_entities_filtered{name=elevator_name.."-input"}) do
-			local already_linked = false
-			for i,v in ipairs(global[link_key]) do
-				if v[1] == e or v[2] == e then already_linked = true end
-			end
-			if not already_linked then
+		for _,e in ipairs(get_oversurface(player.surface).find_entities_filtered{name=elevator_name}) do
+			if not is_linked(e) then
 				global.placement_indicators[player.index] = global.placement_indicators[player.index] or {}
 				table.insert(global.placement_indicators[player.index], rendering.draw_circle{color={0.5, 0.5, 0.5, 0.1}, surface=player.surface, target=e.position, radius=0.3, players={player}})
 			end
 		end
 	end
-	for _,e in ipairs(get_subsurface(player.surface).find_entities_filtered{name=elevator_name.."-input"}) do
-		local already_linked = false
-		for i,v in ipairs(global[link_key]) do
-			if v[1] == e or v[2] == e then already_linked = true end
-		end
-		if not already_linked then
+	for _,e in ipairs(get_subsurface(player.surface).find_entities_filtered{name=elevator_name}) do
+		if not is_linked(e) then
 			global.placement_indicators[player.index] = global.placement_indicators[player.index] or {}
 			table.insert(global.placement_indicators[player.index], rendering.draw_circle{color={0.5, 0.5, 0.5, 0.1}, surface=player.surface, target=e.position, radius=0.3, players={player}})
 		end
@@ -93,17 +79,17 @@ end
 function elevator_on_cursor_stack_changed(player)
 	if player.cursor_stack.valid_for_read then
 		local link_key = false
-		if player.cursor_stack.name == "fluid-elevator" then show_placement_indicators(player, "fluid-elevator", "fluid_elevators")
-		elseif player.cursor_stack.name == "item-elevator" then show_placement_indicators(player, "item-elevator", "item_elevators")
+		if player.cursor_stack.name == "fluid-elevator" then show_placement_indicators(player, "fluid-elevator-input")
+		elseif player.cursor_stack.name == "item-elevator" then show_placement_indicators(player, "item-elevator")
 		elseif player.is_cursor_blueprint() and player.get_blueprint_entities() then
 			local item = false
 			local fluid = false
 			for _,e in ipairs(player.get_blueprint_entities()) do
 				if e.name == "fluid-elevator-input" then fluid = true end
-				if e.name == "item-elevator-input" then item = true end
+				if e.name == "item-elevator" then item = true end
 			end
-			if fluid then show_placement_indicators(player, "fluid-elevator", "fluid_elevators") end
-			if item then show_placement_indicators(player, "item-elevator", "item_elevators") end
+			if fluid then show_placement_indicators(player, "fluid-elevator-input") end
+			if item then show_placement_indicators(player, "item-elevator") end
 		end
 	end
 end
@@ -116,71 +102,76 @@ function elevator_rotated(entity, previous_direction)
 				global.fluid_elevators[i] = {switch_elevator(v[2]), switch_elevator(v[1])}
 			end
 		end
-	elseif entity.name == "item-elevator-input" or entity.name == "item-elevator-output" then
-		for i,v in ipairs(global.item_elevators) do
-			if v[1] == entity or v[2] == entity then
-				global.item_elevators[i] = {switch_elevator(v[2]), switch_elevator(v[1])}
-			end
-		end
+	elseif (entity.name == "item-elevator" or entity.ghost_name == "item-elevator") and entity.linked_belt_neighbour then
+		local neighbour = entity.linked_belt_neighbour
+		entity.disconnect_linked_belts()
+		switch_elevator(entity)
+		switch_elevator(neighbour)
+		entity.connect_linked_belts(neighbour)
+		entity.rotate{reverse=true}
 	end
 end
 
-function elevator_built(entity, link_key, tags)
-	local done = false -- priority is connecting to oversurface
-	if get_oversurface(entity.surface) then
-		for _,e in ipairs(get_oversurface(entity.surface).find_entities_filtered{name=entity.name}) do
-			if e.position.x == entity.position.x and e.position.y == entity.position.y then -- switch to output and link
-				local inp = e
-				local out = entity
-				if tags and tags.type == 1 then -- the built one has to be input, so switch the other one
-					inp = entity
-					out = switch_elevator(e)
+function elevator_built(entity, tags)
+	local iter = {get_oversurface(entity.surface), get_subsurface(entity.surface)}
+	for i=1,2,1 do
+		if iter[i] then
+			local e = iter[i].find_entity(entity.name, entity.position)
+			if e then
+				if entity.name == "fluid-elevator-input" then
+					local inp = e
+					local out = entity
+					if tags and tags.type == 1 then -- the built one has to be input, so switch the other one
+						inp = entity
+						out = switch_elevator(e)
+					else
+						out = switch_elevator(entity)
+					end
+					table.insert(global.fluid_elevators, {inp, out})
 				else
-					out = switch_elevator(entity)
+					if not entity.linked_belt_neighbour then
+						entity.linked_belt_type = "output"
+						entity.connect_linked_belts(e)
+					end
 				end
-				table.insert(global[link_key], {inp, out})
-				done = true
-				break
-			end
-		end
-	end
-	if not done then
-		for _,e in ipairs(get_subsurface(entity.surface).find_entities_filtered{name=entity.name}) do
-			if e.position.x == entity.position.x and e.position.y == entity.position.y then -- switch to output and link
-				local inp = e
-				local out = entity
-				if tags and tags.type == 1 then -- the built one has to be input, so switch the other one
-					inp = entity
-					out = switch_elevator(e)
-				else
-					out = switch_elevator(entity)
-				end
-				table.insert(global[link_key], {inp, out})
 				break
 			end
 		end
 	end
 end
 
-function elevator_selected(player, entity, link_key)
+function elevator_selected(player, entity)
 	global.selection_indicators[player.index] = global.selection_indicators[player.index] or {}
-	for i,v in ipairs(global[link_key]) do
-		if v[1] == entity then
-			if get_subsurface(v[1].surface) == v[2].surface then -- selected entity is input and on top surface
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_line", surface=entity.surface, target=entity, target_offset={0, -0.3}, players={player}})
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_arrow", surface=entity.surface, target=entity, orientation=0.5, players={player}})
-			else -- selected entity is input and on bottom surface
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_line", surface=entity.surface, target=entity, target_offset={0, 0.3}, players={player}})
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_arrow", surface=entity.surface, target=entity, players={player}})
-			end
-		elseif v[2] == entity then
-			if get_subsurface(v[2].surface) == v[1].surface then -- selected entity is output and on top surface
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_line", surface=entity.surface, target=entity, target_offset={0, -0.3}, players={player}})
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_arrow", surface=entity.surface, target=entity, players={player}})
-			else -- selected entity is output and on bottom surface
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_line", surface=entity.surface, target=entity, target_offset={0, 0.3}, players={player}})
-				table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_arrow", surface=entity.surface, target=entity, orientation=0.5, players={player}})
+	local offs = 0 -- line offset, when 0 then don't show indicators
+	local orien = 0 -- arrow orientation (0 is arrow pointing north)
+	if entity.name == "item-elevator" and entity.linked_belt_neighbour and entity.linked_belt_neighbour.name == "item-elevator" then
+		if entity.linked_belt_neighbour.surface == get_subsurface(entity.surface) then -- selected entity is top
+			offs = -0.3
+			if entity.linked_belt_type == "input" then orien = 0.5 end
+		else offs = 0.3
+			if entity.linked_belt_type == "output" then orien = 0.5 end
+		end
+	elseif entity.name == "fluid-elevator-input" or entity.name == "fluid-elevator-output" then
+		for i,v in ipairs(global.fluid_elevators) do
+			if v[1] == entity then
+				if get_subsurface(v[1].surface) == v[2].surface then -- selected entity is input and on top surface
+					offs = -0.3
+					orien = 0.5
+				else -- selected entity is input and on bottom surface
+					offs = 0.3
+				end
+			elseif v[2] == entity then
+				if get_subsurface(v[2].surface) == v[1].surface then -- selected entity is output and on top surface
+					offs = -0.3
+				else -- selected entity is output and on bottom surface
+					offs = 0.3
+					orien = 0.5
+				end
 			end
 		end
+	end
+	if offs ~= 0 then
+		table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_line", surface=entity.surface, target=entity, target_offset={0, offs}, players={player}})
+		table.insert(global.selection_indicators[player.index], rendering.draw_sprite{sprite="utility/indication_arrow", surface=entity.surface, target=entity, orientation=orien, players={player}})
 	end
 end
