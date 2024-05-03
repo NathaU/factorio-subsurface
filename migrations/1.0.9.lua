11041f11a6c74fcbd9329e6b6fabd494559e0e6c
Natha <no-reply@factorio.com>
2024-05-03 18:34:42 +0200

    Changed validation of map limits. Generated tiles are replaced by out-of-map, but remain hidden, which we can use to check if a position is inside the map limits.

--- migrations/1.0.9.lua
+++ migrations/1.0.9.lua
@@ -2,7 +2,7 @@ for _,v in ipairs(global.air_vents) do
 	v.operable = true
 end
 
--- Autoplace manipulation in games that had an older version of this mod
+-- mapgen manipulation in games that had an older version of this mod and adaption to new clear_subsurface mechanics
 for _,s in pairs(game.surfaces) do
 	if is_subsurface(s) then
 		local top = get_oversurface(s)
@@ -10,6 +10,18 @@ for _,s in pairs(game.surfaces) do
 		mgs.seed = top.map_gen_settings.seed
 		mgs.autoplace_controls = make_autoplace_controls(top.name, get_subsurface_depth(s))
 		s.map_gen_settings = mgs
+		
+		local new_tiles = {}
+		for _,t in ipairs(s.find_tiles_filtered{name="grass-1"}) do
+			table.insert(new_tiles, {name = "out-of-map", position = {t.position.x, t.position.y}})
+		end
+		s.set_tiles(new_tiles)
+		for _,t in ipairs(s.find_tiles_filtered{name="out-of-map"}) do
+			if (math.abs(t.position.x) < s.map_gen_settings.width / 2 and math.abs(t.position.y) < s.map_gen_settings.height / 2)
+			or (remote.interfaces["space-exploration"] and math.sqrt(t.position.x^2 + t.position.y^2) < remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = s.index}).radius - 3) then
+				s.set_hidden_tile(t.position, "caveground")
+			end
+		end
 	else
 		manipulate_autoplace_controls(s)
 	end
