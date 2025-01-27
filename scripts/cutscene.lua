e7184a5413d06fc4b807ec8fe7b66a5b432540e0
NathaU <72204083+NathaU@users.noreply.github.com>
2025-01-27 20:22:44 +0100

    Placement indicators are shown after cutscene. Cutscene event changes

--- scripts/cutscene.lua
+++ scripts/cutscene.lua
@@ -6,10 +6,9 @@ script.on_event(defines.events.on_player_driving_changed_state, function(event)
 	end
 end)
 
-script.on_event(defines.events.on_cutscene_waypoint_reached, function(event)
+script.on_event(defines.events.on_cutscene_finished, function(event)
 	local player = game.get_player(event.player_index)
-	if event.waypoint_index == 1 and player.cutscene_character.vehicle then -- if this is the first part of the whole cutscene
-		player.exit_cutscene()
+	if player.vehicle and (player.vehicle.name == "tunnel-entrance" or player.vehicle.name == "tunnel-exit") then -- if this is the first part of the whole cutscene
 		local endpoint_name = player.vehicle.name
 		player.teleport({player.position.x, player.position.y + 1.1}, storage.car_links[player.vehicle.unit_number].surface)
 		local start = player.position
@@ -18,12 +17,15 @@ script.on_event(defines.events.on_cutscene_waypoint_reached, function(event)
 		else
 			start.y = start.y + 2
 		end
-		player.set_controller{type = defines.controllers.cutscene, waypoints={{position = player.position, transition_time = 30, time_to_wait = 0}}, start_position = start}
+		player.set_controller{type = defines.controllers.cutscene, waypoints = {{position = player.position, transition_time = 30, time_to_wait = 0}}, start_position = start}
 		if remote.interfaces["jetpack"] then
 			if is_subsurface(player.surface) then remote.call("jetpack", "block_jetpack", {character = player.cutscene_character})
 			else remote.call("jetpack", "unblock_jetpack", {character = player.cutscene_character}) end
 		end
+		
 	end
+
+	elevator_on_cursor_stack_changed(player)
 end)
 
 script.on_event("subsurface-driving", function(event)
