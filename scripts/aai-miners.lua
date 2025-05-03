5e5c865cf555e65cb9735ecf64c822b39108117e
Natha <.>
2025-05-03 20:44:09 +0200

    clear_subsurface returns an inventory that contains wall mining results

--- scripts/aai-miners.lua
+++ scripts/aai-miners.lua
@@ -92,23 +92,30 @@ function handle_miners(tick)
 				local center_small_excavation = math2d.position.add(center_big_excavation, math2d.vector.from_orientation(orientation, 1.5))
 				local speed_test_position = math2d.position.add(center_small_excavation, math2d.vector.from_orientation(orientation, 1.6))
 				
-				local walls_dug = clear_subsurface(subsurface, center_small_excavation, 1, nil)
-				walls_dug = walls_dug + clear_subsurface(subsurface, center_big_excavation, 3, nil)
+				local inv_1 = clear_subsurface(subsurface, center_small_excavation, 1, nil, true)
+				local inv_2 = clear_subsurface(subsurface, center_big_excavation, 3, nil, true)
 				
-				if walls_dug > 0 then
-					local stack = {name = "stone", count = 20 * walls_dug}
-					local actually_inserted = miner.insert(stack)
-					if actually_inserted ~= stack.count then
-						stack.count = stack.count - actually_inserted
-						subsurface.spill_item_stack{position = miner.position, stack = stack}
+				if not inv_1.is_empty() or not inv_2.is_empty() then
+					for _, inv in ipairs({inv_1, inv_2}) do
+						for _, v in ipairs(inv.get_contents()) do
+							local actually_inserted = miner.insert(v)
+							if actually_inserted ~= v.count then
+								v.count = v.count - actually_inserted
+								subsurface.spill_item_stack{position = miner.position, stack = v}
+							end
+						end
 					end
-					subsurface.pollute(center_small_excavation, walls_dug * 0.25, miner.name)
+					
+					subsurface.pollute(center_small_excavation, (inv_1.get_item_count() + inv_2.get_item_count()) * 0.01, miner.name)
 					subsurface.create_trivial_smoke{name = "fire-smoke-without-glow", position = speed_test_position}
 					subsurface.create_trivial_smoke{name = "fire-smoke-without-glow", position = speed_test_position}
 					subsurface.create_trivial_smoke{name = "fire-smoke-without-glow", position = speed_test_position}
 				end
-
-				if miner.speed > 0 and surface.find_entities_filtered{name = subsurface_wall_names, position = speed_test_position}[1] then
+				
+				inv_1.destroy()
+				inv_2.destroy()
+				
+				if miner.speed > 0 and subsurface.find_entities_filtered{name = subsurface_wall_names, position = speed_test_position}[1] then
 					miner.friction_modifier = 7
 				else
 					miner.friction_modifier = 1
