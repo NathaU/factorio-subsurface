e3b0e0036ba6a1949396753cacd6c47b046c4006
NathaU <72204083+NathaU@users.noreply.github.com>
2025-10-25 11:23:36 +0200

    Locomotives now produce pollution when using chemical fuel

--- settings.lua
+++ settings.lua
@@ -6,14 +6,6 @@ data:extend({
 	default_value = true
   },
   {
-	type = "string-setting",
-	setting_type = "startup",
-	name = "pollution-trains-excludes",
-	default_value = "electric-locomotive,deg-electric-locomotive",
-	allow_blank = true,
-	auto_traim = true
-  },
-  {
 	type = "int-setting",
 	setting_type = "runtime-global",
 	name = "subsurface-limit",
