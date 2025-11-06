6ddd7e9825fa83c71b9ff378c0d4513f6e57872c
NathaU <72204083+NathaU@users.noreply.github.com>
2025-11-06 16:11:22 +0100

    Apply global effects only with challenges enabled. Removed pollution effect.

--- migrations/1.1.14.lua
+++ migrations/1.1.14.lua
@@ -1,5 +1,7 @@
 for _, s in pairs(storage.subsurfaces) do
 	local effect = s.global_effect
 	effect.quality = nil
+	effect.pollution = nil
 	s.global_effect = effect
+	if settings.global["enable-challenges"].value then s.global_effect = nil end
 end
