1c5826796f19f9860661cc37b63884899fa2f14d
Natha <.>
2026-01-08 22:33:08 +0100

    Merged pole_links and car_links into tunnel_links and removed invalid entries. Fixed #46

--- migrations/1.1.0.lua
+++ migrations/1.1.0.lua
@@ -1,10 +1,3 @@
-for _, e in pairs(storage.pole_links) do
-	e.destructible = false
-end
-for _, e in pairs(storage.car_links) do
-	e.destructible = false
-end
-
 for _, s in pairs(game.surfaces) do
 	for _, e in ipairs(s.find_entities_filtered{name = {"subsurface-wall-map-border", "tunnel-entrance-sealed-0", "tunnel-entrance-sealed-1", "tunnel-entrance-sealed-2", "tunnel-entrance-sealed-3"}}) do
 		e.destructible = false
