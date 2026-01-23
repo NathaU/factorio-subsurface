44f578fe77b04641173c1fc745b8723e4ff338cc
Natha <.>
2026-01-23 19:23:37 +0100

    Tips and tricks entry for subways

--- prototypes/tips-and-tricks.lua
+++ prototypes/tips-and-tricks.lua
@@ -171,5 +171,139 @@ data:extend({
 			}}
 		]]
 	},
+  },
+  {
+	type ="tips-and-tricks-item",
+	name = "subways",
+	category ="subsurface",
+	order = "d",
+	tag = "[entity=subway]",
+	indent = 1,
+	dependencies = {"subsurface"},
+	trigger = {type = "research", technology = "inter-surface-transport"},
+	simulation = {
+		mods = {"Subsurface"},
+		save = "__Subsurface__/simulations/tips-and-tricks-default.zip",
+		init = [[	
+			require("__core__/lualib/story")
+			local surface = game.get_surface(2)
+			
+			remote.call("subsurface", "expose", surface, {0, 5}, 5)
+			remote.call("subsurface", "expose", surface, {-5, 5}, 5)
+			remote.call("subsurface", "expose", surface, {-10, 5}, 5)
+			remote.call("subsurface", "expose", surface, {-15, 5}, 5)
+			remote.call("subsurface", "expose", surface, {-20, 5}, 5)
+
+			local player = game.simulation.create_test_player{name = ""}
+
+			surface.create_entity{name = "subway", position = {-5, 5}, direction = defines.direction.east, raise_built = true, force = "player"}
+			surface.create_entities_from_blueprint_string{
+				string = "0eNqN0ssKgzAQBdB/mXWE+oht8iulFB+DHdAoSSwVyb83KqWLUphlJveezcwKdT/jZMl40CtQMxoH+rqCo85U/TYz1YCgwVbUQxBApsUX6DTcBKDx5AmPxv5Y7mYearQxID5N52O3e/hkJwRMo4ut0Wx4lJIsRhfQMuItWWyOvyKIHzNjm6nimjnfPHPNgm9Krin5Zs41S775Z0fxCMjjEIXvHQl4onV7QJaZKpSSlzRX6lSE8AaZWM3C",
+				position = {-16, 5},
+			}
+			game.get_surface(1).create_entity{name = "subway", position = {9, 5}, direction = defines.direction.west, raise_built = true, force = "player"}
+			game.get_surface(1).create_entities_from_blueprint_string{
+				string = "0eNqN0ttqwzAMBuBXKbp2YTm4qf0cvRtjuKnWGhI52M5YCX732QnbYKWgO8vS/2GwFjgPM07eUgS9gO0dBdCvCwR7JTOUOzIjggZv7ABJgKULfoGu0psApGijxS2xFvd3mscz+jwgfpIh5uz1FvcrIWByIaccFbxIUsAdtMz2xXrst1abxANZs8mOSzZsUnHJlkvWFZeUbLLhkgc2yf6e7pcsIu1DdNNTr/vnifwOs53h5KZdmP2H6XG3ImXXbMQx9/7WVcAn+rAm5KFWrVLyWDVKvbQpfQNuE/E3",
+				position = {20, 5},
+			}
+			local stop = game.get_surface(1).create_entity{name = "train-stop", station = "Top surface stop", position = {25, 7}, direction = defines.direction.east, raise_built = true, force = "player"}
+			local loco
+
+			tip_story_init{{
+			  {
+				name = "start",
+			 	init = function()
+					player.teleport({-17.5, 7}, surface)
+					game.simulation.camera_player = player
+					game.simulation.camera_position = {-10, 5}
+					loco = surface.create_entity{name = "locomotive", copy_color_from_train_stop = false, position = {-18.5, 5}, orientation = 0.25, force = player.force}
+					loco.insert("wood")
+				end
+			  },
+			  {condition = function() return game.simulation.move_cursor{position = loco.position, speed = 0.2} end},
+			  {
+			  	condition = story_elapsed_check(0.25),
+				action = function() game.simulation.control_press{control = "open-gui", notify = false} end
+			  },
+			  {condition = story_elapsed_check(0.25)},
+			  {
+				condition = function()
+					local target = game.simulation.get_widget_position({type = "text-button-localised-substring", data = "gui-train.add-station"})
+					return game.simulation.move_cursor{position = target, speed = 0.1}
+				end,
+			  },
+			  {
+			  	condition = story_elapsed_check(0.25),
+				action = function() game.simulation.mouse_click() end
+			  },
+			  {condition = story_elapsed_check(0.25)},
+			  {
+				condition = function()
+					local target = game.simulation.get_widget_position({type = "text-button-substring", data = "0 [img=elevator] 1"})
+					return game.simulation.move_cursor{position = target, speed = 0.1}
+				end,
+			  },
+			  {
+			  	condition = story_elapsed_check(0.25),
+				action = function() game.simulation.mouse_click() end
+			  },
+			  {
+				condition = function()
+					local target = game.simulation.get_widget_position({type = "text-button-localised-substring", data = "gui-train.add-station"})
+					return game.simulation.move_cursor{position = target, speed = 0.1}
+				end,
+			  },
+			  {
+			  	condition = story_elapsed_check(0.25),
+				action = function() game.simulation.mouse_click() end
+			  },
+			  {condition = story_elapsed_check(0.25)},
+			  {
+				condition = function()
+					local target = game.simulation.get_widget_position({type = "text-button-substring", data = "Top surface stop"})
+					return game.simulation.move_cursor{position = target, speed = 0.1}
+				end,
+			  },
+			  {
+			  	condition = story_elapsed_check(0.25),
+				action = function() game.simulation.mouse_click() end
+			  },
+			  {
+				condition = story_elapsed_check(1),
+				action = function() loco.set_driver(player.character) end
+			  },
+			  {
+				condition = function()
+					local target = game.simulation.get_widget_position({type = "train-schedule-action-button"})
+					return game.simulation.move_cursor{position = target, speed = 0.1}
+				end,
+			  },
+			  {
+			  	condition = story_elapsed_check(0.25),
+				action = function()
+					game.simulation.mouse_click()
+					player.opened = nil
+					--local schedule = loco.train.schedule
+					--schedule.records[2].wait_conditions = {{type = "inactivity", ticks = 300}}
+					--loco.train.schedule = schedule
+				end
+			  },
+			  {
+				condition = function() return (game.train_manager.get_trains{surface = 1}[1] or {}).station ~= nil end,
+				update = function()
+					game.simulation.camera_surface_index = player.character.surface.index
+					game.simulation.camera_position = player.character.position
+				end
+			  },
+			  {
+				condition = story_elapsed_check(3),
+				action = function()
+					game.train_manager.get_trains{surface=1}[1].carriages[1].destroy()
+					story_jump_to(storage.story, "start")
+				end
+			  }
+			}}
+		]]
+	},
   }
 })
